4af6b3b1cf28146b129ffd1602aabfa821760671
Jeffrey McAteer <jeffrey@jmcateer.com>
2024-02-24 10:47:11 -0500

    yay hello world compiles! intels compiler distribution chain is a mess though.

--- build.py
+++ build.py
@@ -4,6 +4,7 @@ import os
 import sys
 import subprocess
 import shutil
+import traceback
 
 def cmd(*args, **kwargs):
   if not 'check' in kwargs:
@@ -25,27 +26,41 @@ def ensure_intel_fortran_avail():
   if not shutil.which('ifx'):
     raise Exception('Please download the intel HPC kit and ensure ifx is on your PATH')
 
+def ostyped(linux_val, windorks_val):
+  if os.name == 'nt':
+    return windorks_val
+  return linux_val
 
 def main(args=sys.argv):
   os.makedirs('bin', exist_ok=True)
 
   ensure_intel_fortran_avail()
 
-  main_bin = os.path.abspath(os.path.join('bin', 'main'))
-  alib_bin = os.path.abspath(os.path.join('bin', 'alib'))
+  main_bin = os.path.abspath(os.path.join('bin', 'main'))+ostyped('', '.exe')
+  alib_dynamic_bin = os.path.abspath(os.path.join('bin', 'alib'))+ostyped('.so', '.dll')
+  alib_static_bin = os.path.abspath(os.path.join('bin', 'alib'))+ostyped('.a', '.lib')
+  alib_static_obj = os.path.abspath(os.path.join('bin', 'alib'))+ostyped('.o', '.o')
+
   warning_args = [
     '-warn', 'all',
     '-check', 'all',
     '-g', '-traceback',
   ]
 
-  cmd('ifx', *warning_args, '-o', alib_bin, '-shared', 'alib.f90')
+  cmd('ifx', *warning_args, '-o', alib_dynamic_bin, '-shared', '-fpic', 'alib.f90')
 
-  cmd('ifx', *warning_args, '-o', main_bin, 'main.f90', '-Bstatic', alib_bin)
+  cmd('ifx', *warning_args, '-c', '-Fo'+alib_static_obj, '-fpie', '-fpic', 'alib.f90')
+  cmd('ar', 'cr', alib_static_bin, alib_static_obj)
+
+  cmd('ifx', *warning_args, '-o', main_bin, 'main.f90', '-Bstatic', alib_static_bin)
 
   cmd(main_bin, '')
 
 
 
 if __name__ == '__main__':
-  main()
+  try:
+    main()
+  except:
+    if not 'CalledProcessError' in traceback.format_exc():
+      traceback.print_exc()
